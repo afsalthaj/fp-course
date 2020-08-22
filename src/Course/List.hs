59ef47a3bc9039b9084aec01207904c34279d80d
d948521 <afsal.taj06@gmail.com>
2020-08-22 23:45:31 +1000

    Implement flatten with point free

--- src/Course/List.hs
+++ src/Course/List.hs
@@ -201,7 +201,7 @@ infixr 5 ++
 flatten ::
   List (List a)
   -> List a
-flatten = foldRight (\xs acc -> xs ++ acc) Nil
+flatten = foldRight (++) Nil
 
 -- | Map a function then flatten to a list.
 --
