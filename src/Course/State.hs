305f3a642dab5cbcaafba456227c12fb4dadb66c
d948521 <afsal.taj06@gmail.com>
2020-10-17 23:53:44 +1100

    Add put get eval and exec for state

--- src/Course/State.hs
+++ src/Course/State.hs
@@ -38,8 +38,9 @@ exec ::
   State s a
   -> s
   -> s
-exec =
-  error "todo: Course.State#exec"
+exec st s =
+  let (_, s') = runState st s
+  in s'
 
 -- | Run the `State` seeded with `s` and retrieve the resulting value.
 --
@@ -48,8 +49,9 @@ eval ::
   State s a
   -> s
   -> a
-eval =
-  error "todo: Course.State#eval"
+eval st s =
+  let (a, _) = runState st s
+  in a
 
 -- | A `State` where the state also distributes into the produced value.
 --
@@ -57,8 +59,7 @@ eval =
 -- (0,0)
 get ::
   State s s
-get =
-  error "todo: Course.State#get"
+get = State(\s -> (s, s))
 
 -- | A `State` where the resulting state is seeded with the given value.
 --
